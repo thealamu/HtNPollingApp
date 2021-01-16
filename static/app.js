3d46f1595a8241a04e51a705a71a1dab9b0ea236
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-16 23:25:03 +0100

    Extract function to update index page bars

--- static/app.js
+++ static/app.js
@@ -1,37 +1,48 @@
-
-var pollMembers = document.querySelectorAll('.poll-member')
-var members = ['Go', 'Python', 'PHP', 'Ruby']
+var pollMembers = document.querySelectorAll(".poll-member");
+var members = ["Go", "Python", "PHP", "Ruby"];
 
 // Sets up click events for all the cards on the DOM
 pollMembers.forEach((pollMember, index) => {
-  pollMember.addEventListener('click', (event) => {
-    handlePoll(members[index])
-  }, true)
-})
+  pollMember.addEventListener(
+    "click",
+    (event) => {
+      handlePoll(members[index]);
+    },
+    true
+  );
+});
 
 // Sends a POST request to the server using axios
-var handlePoll = function(member) {
-  axios.post('http://localhost:5000/vote', {member}).then((r) => console.log(r))
-}
+var handlePoll = function (member) {
+  axios
+    .post("http://localhost:5000/vote", { member })
+    .then((r) => console.log(r));
+};
 
 // Configure Pusher instance
-const pusher = new Pusher('4ac52c799ded4e369788', {
-  cluster: 'us2',
-  encrypted: true
+const pusher = new Pusher("4ac52c799ded4e369788", {
+  cluster: "us2",
+  encrypted: true,
 });
 
 // Subscribe to poll trigger
-var channel = pusher.subscribe('poll');
+var channel = pusher.subscribe("poll");
+
+var updateMembers = function (data) {
+  for (i = 0; i < data.length - 1; i++) {
+    var total = data[0].votes + data[1].votes + data[2].votes + data[3].votes;
+    document.getElementById(data[i].name).style.width = calculatePercentage(
+      total,
+      data[i].votes
+    );
+    document.getElementById(data[i].name).style.background = "#388e3c";
+  }
+};
 
 // Listen to vote event
-channel.bind('vote', function(data) {
-  for (i = 0; i < (data.length - 1); i++) { 
-    var total = data[0].votes + data[1].votes + data[2].votes + data[3].votes
-    document.getElementById(data[i].name).style.width = calculatePercentage(total, data[i].votes)
-    document.getElementById(data[i].name).style.background = "#388e3c" 
-  }
-});
+channel.bind("vote", updateMembers);
 
-let calculatePercentage = function(total, amount) {
-  return (amount / total) * 100 + "%"
-}
+let calculatePercentage = function (total, amount) {
+  return (amount / total) * 100 + "%";
+};
+
